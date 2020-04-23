81eee25887c69483ebc9ae0df1889381da62c790
polmes <pol.developer1@gmail.com>
2020-04-22 18:32:25 -0600

    Test script now takes Apollo mass properties

--- run.m
+++ run.m
@@ -1,18 +1,25 @@
-% Input
-earth = Planet(6371e3, 5.97237e24, AtmosphereStd76);
-apollo = Spacecraft(1000, 1, 2/3*100*1^2 * ones(1, 3), 'apollo.mat');
-T = 3600 * 10; % max integration time
+% Earth Planet
+earth = Planet(6371e3, 5.97237e24, AtmosphereCOESA);
+
+% Apollo Spacecraft
+% Reference diameter/area from Moss (DSMC Simulations)
+% Mass properties from "CsmLmSpacecraftOperationalDataBook-Volume3-MassProperties"
+% Weight: lb -> kg, inertia moments: slug.ft^2 -> kg.m^2
+apollo = Spacecraft(5856.8754, 3.9116, [8022.3748, 7149.22804, 6421.1538], 'apollo.mat');
+
+% Max integration time
+T = 3600 * 2; % 3600 * 10;
 
 % Initial conditions
 alt = 120e3;
-Uinf = 12e3; % 10.73e3;
-gamma = deg2rad(-5); % deg2rad(-5.9);
+Uinf = 10e3; % 10.73e3;
+gamma = deg2rad(-6); % deg2rad(-5.9);
 S0 = [alt, Uinf, gamma];
 % S0 = [];
 
 % Trajectory simulation
-engine = Engine; % keep defaults
+engine = Engine('Reltol', 1e-3, 'AbsTol', 1e-5);
 [t, S, ie] = engine.integrate(T, S0, apollo, earth);
 
 % Post
-util.visualize(S, earth);
+util.visualize(t, S, earth);
