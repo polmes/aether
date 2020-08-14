11636a357ad30e88579edcc974b816bbc6b78860
polmes <pol.developer1@gmail.com>
2020-08-13 22:10:07 -0600

    Finally upload GRAM atmospheric model
    Note that this uses the NASA-provided Earth GRAM 2016 code
    (designed to be used with the original executable without modification)

--- Atmosphere.m
+++ Atmosphere.m
@@ -3,14 +3,16 @@ classdef Atmosphere < handle
 	% Base class for atmospheric models
 
 	properties (Constant)
+		% Atmosphere (edge) limit = Entry Interface (EI)
+		lim = 120e3; % [m]
+	end
+
+	properties (Access = private, Constant)
 		% Universal Gas Constant
 		Ru = 8.31446261815324; % [J/(mol.K)]
 
 		% Avogadro's Number
 		NA = 6.02214076e23; % [particles/mol]
-
-		% Atmosphere (edge) limit = Entry Interface (EI)
-		lim = 120e3; % [m]
 	end
 
 	methods (Abstract)
