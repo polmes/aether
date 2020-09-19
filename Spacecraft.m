b35eb84fb38a4d1e146ec94772da5daf648d4548
polmes <pol.developer1@gmail.com>
2020-09-19 16:37:43 -0600

    Support for non-diagonal inertia tensors

--- Spacecraft.m
+++ Spacecraft.m
@@ -21,8 +21,24 @@ classdef Spacecraft < handle
 				self.m = data.mass;
 				self.L = data.length;
 				self.R = data.nose;
-				self.I = diag(data.inertia);
 				self.deploy = data.deploy;
+
+				% Inertia can be either diagonal or full tensor
+				I = data.inertia;
+				if numel(I) == 3
+					% [Ixx; Iyy; Izz]
+					self.I = diag(data.inertia);
+				elseif numel(I) == 6
+					% [Ixx; Iyy; Izz; Ixy; Iyz; Ixz]
+					self.I = diag(I(1:3)) + diag(I(4:5), 1) + diag(I(4:5), -1) + diag(I(6), 2) + diag(I(6), -2);
+				elseif numel(I) == 9
+					% [Ixx; Ixy; Ixz; Ixy; Iyy; Iyz; Ixz; Iyz; Izz]
+					self.I = reshape(I, [3 3]).';
+				else
+					ME = MException('Spacecraft:constructor', 'Unexpected length of inertia vector');
+					throw(ME);
+				end
+
 
 				% Store database as struct
 				self.db = util.open(data.database, 'DataDir', self.datadir);
