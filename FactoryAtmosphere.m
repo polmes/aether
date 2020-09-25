045f2b783ffebb1cd9bfb16bcb80f58ae6d1082e
polmes <pol.developer1@gmail.com>
2020-09-24 19:20:19 -0600

    Construct Atmosphere models with only dateandtime
    Remove "directory" option from JSON as it is no longer used

--- FactoryAtmosphere.m
+++ FactoryAtmosphere.m
@@ -9,16 +9,12 @@ classdef FactoryAtmosphere < Factory
 			self = self@Factory(varargin);
 		end
 
-		function at = generate(self, atmosphere, dateandtime)
+		function at = generate(self, model, dateandtime)
 			%   atmosphere  :=  'atmosphere' structure from case *.json
 			%   dateandtime :=  Datetime string, e.g. '1967/11/09 20:00:00'
 
-			% Append additional information to constructor input argument
-			argdata = atmosphere;
-			argdata.dateandtime = dateandtime;
-
 			% Generate Atmosphere (note: first argument defines class)
-			at = self.constructor(atmosphere.model, argdata);
+			at = self.constructor(model, dateandtime);
 		end
 	end
 
