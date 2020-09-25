765830736d8ed72d3a75b14d21107fa4d4385fb2
polmes <pol.developer1@gmail.com>
2020-09-24 22:44:26 -0600

    Turn AtmosphereStochastic into factory

---
+++ AtmosphereStochastic.m
@@ -0,0 +1,32 @@
+classdef AtmosphereStochastic < Factory
+	properties (Constant, Access = protected)
+		defaults = {'GRAM', @nasa.AtmosphereStochasticGRAM, 'FastGRAM', @nasa.AtmosphereStochasticFastGRAM};
+		exception = 'Unkown stochastic atmosphere model provided';
+	end
+
+	methods
+		function self = AtmosphereStochastic(varargin)
+			self = self@Factory(varargin);
+		end
+
+		function at = generate(self, obj)
+			%   obj :=  (Non-stochastic) Atmosphere object of a supported class
+
+			% Generate (stochastic) Atmosphere (note: first argument defines class)
+			cls = class(obj);
+			model = cls((strfind(cls, 'Atmosphere') + numel('Atmosphere')):end);
+			at = self.constructor(model, obj);
+		end
+	end
+
+	methods (Static)
+		% Static property: DICTIONARY
+		function out = dictionary(in)
+			persistent DICTIONARY;
+			if nargin
+				DICTIONARY = in;
+			end
+			out = DICTIONARY;
+		end
+	end
+end
