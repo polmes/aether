78fa6983f1ae9f9e374f4de120fd6ffa5eeb551d
polmes <pol.developer1@gmail.com>
2020-10-28 15:01:41 -0600

    Compute quantities w.r.t. relative velocity now

--- +util/getQoI.m
+++ +util/getQoI.m
@@ -1,6 +1,6 @@
 function QoI = getQoI(t, S, ie, sc, pl)
 	% Constants
-	%    Umag [m/s], f
+	%    Uinf [m/s], f
 	f = [0         , 0    ;
 	     8999      , 0    ;
 	     9000      , 1.5  ;
@@ -25,11 +25,46 @@ function QoI = getQoI(t, S, ie, sc, pl)
 	Cc = 18.8e-5; % convective heating constant [W/m^2]
 	Cr = 4.736e8; % radiative heating constant [W/m^2]
 
-	% Pre (assumes XYZ engine)
+	% Extract variables
 	x = S(:,1); y = S(:,2); z = S(:,3);
-	[lat, lon, alt, rad] = pl.xyz2lla(x, y, z, t);
-	Umag = sqrt(sum(S(:,8:10).^2, 2));
+	q0 = S(:,4); q1 = S(:,5); q2 = S(:,6); q3 = S(:,7);
+	u = S(:,8); v = S(:,9); w = S(:,10);
+	N = numel(t);
+
+	% Pre-processing (assumes XYZ engine)
+	[lat, lon, alt, rad, Lie] = pl.xyz2lla(x, y, z, t);
 	rho = pl.atm.trajectory(t, alt, lat, lon);
+
+	% Common
+	i1 = 1:3:(1+(N-1)*3);
+	i2 = 2:3:(2+(N-1)*3);
+	i3 = 3:3:(3+(N-1)*3);
+	i = [i1, i1, i1, i2, i2, i2, i3, i3, i3];
+	j = [i1, i2, i3, i1, i2, i3, i1, i2, i3];
+
+	% [EV] = V -> E
+	k = [cos(lon)   ;  sin(lat).*sin(lon); -cos(lat).*sin(lon) ;
+	     zeros(N, 1);  cos(lat)          ;  sin(lat)           ;
+	     sin(lon)   ; -sin(lat).*cos(lon);  cos(lat).*cos(lon)];
+	Lve = sparse(i, j, k);
+
+	% [BI] = I -> B
+	k = [q0.^2+q1.^2-q2.^2-q3.^2; 2*(q1.*q2+q0.*q3)      ; 2*(q1.*q3-q0.*q2)       ;
+	     2*(q1.*q2-q0.*q3)      ; q0.^2-q1.^2+q2.^2-q3.^2; 2*(q0.*q1+q2.*q3)       ;
+	     2*(q0.*q2+q1.*q3)      ; 2*(q2.*q3-q0.*q1)      ; q0.^2-q1.^2-q2.^2+q3.^2];
+	Lib = sparse(i, j, k);
+
+	% [IE] = E -> I
+	Lei = Lie.';
+
+	% [BV] = V -> B
+	% [BV] = [BI][IE][EV]
+	Lvb = Lib * Lei * Lve;
+
+	% Velocities
+	Urel = reshape([u, v, w].', [], 1) - Lvb * reshape(cat(2, pl.atmspeed(rad, lat), zeros(N, 1), zeros(N, 1)).', [], 1);
+	Umag = sqrt(sum([u, v, w].^2, 2)); % inertial
+	Uinf = sqrt(sum(reshape(Urel.^2, 3, []))).'; % freestream
 
 	% Status
 	st = ie(end);
@@ -40,25 +75,25 @@ function QoI = getQoI(t, S, ie, sc, pl)
 	% Range
 	ran = pl.greatcircle(rad(1), lat(1), lon(1), rad(end), lat(end), lon(end));
 
-	% Final velocity
-	Uend = Umag(end);
+	% Final velocity (relative)
+	Uend = Uinf(end);
 
-	% Max speed
+	% Max speed (inertial)
 	maxU = max(Umag);
 
 	% Max-g
-	% maxG = max(sqrt(sum((diff(S(:,8:10)) ./ diff(t)).^2, 2)));
+	% maxG = max(sqrt(sum((diff([u, v, w]) ./ diff(t)).^2, 2)));
 	maxG = max(abs(diff(Umag) ./ diff(t)));
 
 	% Max-Q
-	maxQ = max(1/2 * rho .* Umag.^2);
+	maxQ = max(1/2 * rho .* Uinf.^2);
 
 	% Convective heat flux
-	dqc = Cc * sqrt(rho / sc.R) .* Umag.^3;
+	dqc = Cc * sqrt(rho / sc.R) .* Uinf.^3;
 
 	% Radiative heat flux
-	a = 1.072e6 * Umag.^(-1.88) .* rho.^(-0.325);
-	dqr = Cr * sc.R.^a .* rho.^(1.22) .* interp1(f(:,1), f(:,2), Umag);
+	a = 1.072e6 * Uinf.^(-1.88) .* rho.^(-0.325);
+	dqr = Cr * sc.R.^a .* rho.^(1.22) .* interp1(f(:,1), f(:,2), Uinf);
 	dqr(a > 1) = 0; % out-of-range
 
 	% Integrated heat
