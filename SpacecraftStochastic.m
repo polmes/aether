670c7516b1d59ac72a13631dc9d7942da565cbdc
polmes <pol.developer1@gmail.com>
2020-10-28 13:48:12 -0600

    Support new database coefficients
    - Will choose 1 from possible pitching moments ('Cm', 'CmAft', ...)
    - If more than 3 inputs provided, will also affect 'Cmq'
    - If less than 3 inputs provided, will only affect 'CL', 'CD'

--- SpacecraftStochastic.m
+++ SpacecraftStochastic.m
@@ -1,7 +1,11 @@
 classdef SpacecraftStochastic < Spacecraft
 	properties
-		Cn = {'CL', 'CD', 'Cm'}; % Cx's names
+		Cn; % Cx's names
 		mean;
+	end
+
+	properties (Access = protected, Constant)
+		sorted = {'CL', 'CD', {'Cm', 'CmCG', 'CmAft'}, 'Cmq'}; % possible coefficient names in order of priority
 	end
 
 	methods
@@ -10,14 +14,19 @@ classdef SpacecraftStochastic < Spacecraft
 			% sc    :=  Spacecraft object (non-Stochastic)
 			self = self.copy(sc); % @Spacecraft(sc);
 
-			% Save mean values and variances
+			% Get available coefficients
+			allCn = fieldnames(self.db);
+			self.Cn = cell(1, numel(inputs));
+
+			% Save coefficient names and their mean values
 			for i = 1:numel(inputs)
+				self.Cn{i} = cell2mat(intersect(self.sorted{i}, allCn));
 				self.mean.(self.Cn{i}) = self.db.(self.Cn{i});
 			end
 		end
 
 		function update(self, Y)
-			% Add random noise to 3 Cx's: CL, CD, Cm
+			% Add random noise to first N Cx's: CL, CD, Cm, ..
 			for i = 1:numel(Y)
 				self.db.(self.Cn{i}) = self.mean.(self.Cn{i}) * Y(i);
 			end
